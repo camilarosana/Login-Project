cfdb586966e2acd84746e0b60dfc2b34b03f24ba
Camila Rosana <camilarosana95@gmail.com>
2023-09-14 15:21:00 -0300

    Adição das imagens e o script

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const mode = document.querySelector('#mode-icon');
+const form = document.getElementById('login-form');
+mode.addEventListener('click', ()=>{
+
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark');
+        return;
+    }
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+    form.classList.remove('dark');
+});
